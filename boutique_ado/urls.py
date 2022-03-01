e2d579f59c6f768e0511e0ace51ed9751244b654
totalnoMartina <totalnobzvz@yahoo.com>
2022-03-01 21:28:08 +0000

    Add template for all products

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('homeapp.urls')),
+    path('allproducts/', include('allproducts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
